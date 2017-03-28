5367510d9d9316d1daa8294bd5199fd7862a2628
Kirill <mazkirand@gmail.com>
2017-03-28 09:29:07 +0300

    remake, make web app

--- src/by/pvt/mazanov/stone/enums/StoneType.java
+++
@@ -1,6 +0,0 @@
-package by.pvt.mazanov.stone.enums;
-
-
-public  enum StoneType {
-    PRECISIOUS, SEMIPRECISIOUS;
-}
--- src/by/pvt/mazanov/stone/interfaces/TotalAmount.java
+++
@@ -1,9 +0,0 @@
-package by.pvt.mazanov.stone.interfaces;
-
-/**
- * Created by mazanov on 11.03.17.
- */
-public interface TotalAmount {
-    double getCost();
-    double getWeight();
-}
